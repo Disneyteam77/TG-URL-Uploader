e6b00553a965038f21d797373cecf306ca233e3d
ViruZs <52099763+TGExplore@users.noreply.github.com>
2020-11-15 12:16:49 +0530

    Noob 🤭

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -36,7 +36,7 @@ from pyrogram.errors import UserNotParticipant, UserBannedInChannel
 @pyrogram.Client.on_message(pyrogram.Filters.regex(pattern=".*http.*"))
 async def echo(bot, update):
     if update.from_user.id in Config.BANNED_USERS:
-        await update.reply_text("You are B A N N E D")
+        await update.reply_text("You are B A N N E D 🤣🤣🤣🤣")
         return
     TRChatBase(update.from_user.id, update.text, "/echo")
     update_channel = Config.UPDATE_CHANNEL
@@ -44,14 +44,14 @@ async def echo(bot, update):
         try:
             user = await bot.get_chat_member(update_channel, update.chat.id)
             if user.status == "kicked":
-               await update.reply_text("You are B A N N E D in channel")
+               await update.reply_text("🤭 Sorry Dude, You are *#B A N N E D 🤣🤣🤣**")
                return
         except UserNotParticipant:
             #await update.reply_text(f"Join @{update_channel} To Use Me")
             await update.reply_text(
-                text="Join My Updates Channel to use ME 😎",
+                text="**Join My Updates Channel to use ME 😎 🤭**",
                 reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="Join My Update Channel", url=f"https://t.me/{update_channel}")]
+                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
               ])
             )
             return
