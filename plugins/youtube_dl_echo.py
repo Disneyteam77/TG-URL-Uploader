f8d858e56a7b4b40954818664d9264fe910f0fa9
ViruZs <52099763+TGExplore@users.noreply.github.com>
2020-11-15 11:58:20 +0530

    Aa

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -49,7 +49,7 @@ async def echo(bot, update):
         except UserNotParticipant:
             #await update.reply_text(f"Join @{update_channel} To Use Me")
             await update.reply_text(
-                text="Join My Updates Channel to use ME 😎,
+                text="Join My Updates Channel to use ME 😎",
                 reply_markup=InlineKeyboardMarkup([
                     [ InlineKeyboardButton(text="Join My Update Channel", url=f"https://t.me/{update_channel")]
               ])
