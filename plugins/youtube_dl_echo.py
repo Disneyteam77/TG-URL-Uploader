7510b7fdce15c4e486720af0b623ce016fd3921d
ViruZs <52099763+TGExplore@users.noreply.github.com>
2020-11-22 15:28:59 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -288,7 +288,7 @@ async def echo(bot, update):
         )
         logger.info(f"Thumbnail22:{thumb_image_path}")
         im = Image.open(thumb_image_path).convert("RGB")
-        thumb_image_path = im.save(f"{thumb_image_path}.jpg", "jpeg")
+        im.save(f"{thumb_image_path}.jpg", "jpeg")
         logger.info(f"Thumbnail New JPG:{thumb_image_path}")
         await bot.send_message(
             chat_id=update.chat.id,
